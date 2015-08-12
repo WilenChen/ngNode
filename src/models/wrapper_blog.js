e924b11ae040078ddd17fca30318bfa4c0ef1eb2
hcnode <7372@163.com>
2015-08-12 17:52:31 +0800

    edit readme

--- src/models/wrapper_blog.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by harry on 15/7/9.
- */
-window.ModelConf = require("/Users/harry/node/angunode2/angunode-sample/models//blog");
--- src/models/wrapper_user.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by harry on 15/7/9.
- */
-window.ModelConf = require(".//user");
